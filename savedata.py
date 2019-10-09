a8a2f9e4a313535ab19806ead1885c97f9996f09
asedeki <asedeki@gmail.com>
2019-10-10 01:56:29 +0200

    il y du chemin a faire...

--- savedata.py
+++ savedata.py
@@ -1,14 +1,19 @@
+# -*- coding: utf_8 -*-
 import sqlite3 as sq
 from optparse import OptionParser
 import numpy as np
+import concurrent.futures as ccf
+
 # from numba import jit
+
+
 def opt():
     parser = OptionParser()
     parser.add_option("-d", "--database", action="store", dest="database", type="string",
                       default=":memory:", help="""le nom de la base de données
                                        """)
     #
-    parser.add_option("-f",action="store", dest="data_file", type="string")
+    parser.add_option("-f", action="store", dest="data_file", type="string")
     (_val, args) = parser.parse_args()
     return _val
 
@@ -25,42 +30,61 @@ class Quasi1dData():
                            "g2 float default 0.64",
                            "g3 float default 0.0",
                            "Np int default 32",
-                           "error float default 1e-3",
+                           "rel_tol float default 1e-3",
                            "Ef float default 3000",
-                           "time text default NULL"
+                           "time text default NULL",
+                           "remarque text default NULL"
                            ],
-            "INTERACTIONS": ["parametersId int",
-                             "g1 float",
-                             "g2 float",
-                             "g3 float"
-                             ]
+            "INTERACTION": ["parametersId int",
+                            "g1 float",
+                            "g2 float",
+                            "g3 float"
+                            ],
+            "SUSCEPTIBILITY": ["parametersId int",
+                               "CSDW_0 float", "CSDW_pi float",
+                               "CBDW_0 float", "CBDW_pi float",
+                               "SSDW_0 float", "SSDW_pi float",
+                               "SBDW_0 float", "SBDW_pi float",
+                               "SS_s float", "SS_dxy float", "SS_dx2y2 float", "SS_g float", "SS_i float",
+                               "ST_px float", "ST_py float", "ST_h float", "ST_f float"
+                               ]
         }
     }
     parameters = {}
     interactions = {}
+    susceptibilities = {}
 
     def __new__(cls, *vargs, **kwargs):
         if cls.database is None:
             cls.database = super(Quasi1dData, cls).__new__(cls)
         return cls.database
 
-    def __init__(self, database=None, **kwargs):
+    def __init__(self, database, **kwargs):
         for param in self.db_param["table"]["PARAMETERS"]:
-            param = param.split()[0]
-            self.parameters[param] = None
-        for k in self.db_param["table"]["INTERACTIONS"]:
-            k = k.split()[0]
-            self.interactions[k] = None
+            param = param.split()
+            ik = param.index('default') if 'default' in param else None
+            self.parameters[param[0]] = param[ik +
+                                              1] if ik is not None else None
+        for param in self.db_param["table"]["INTERACTION"]:
+            param = param.split()
+            ik = param.index('default') if 'default' in param else None
+            self.interactions[param[0]] = param[ik +
+                                                1] if ik is not None else None
+        for param in self.db_param["table"]["SUSCEPTIBILITY"]:
+            param = param.split()
+            ik = param.index('default') if 'default' in param else None
+            self.susceptibilities[param[0]] = param[ik +
+                                                    1] if ik is not None else None
 
+        self.db_param['name'] = database
         self.parameters.update(kwargs)
-        self.connect(database)
-        self.cursor.execute("PRAGMA foreign_keys = ON")
+        self.connect()
 
-    def connect(self, database):
-        if database is None:
-            database = self.db_param["name"]
+    def connect(self):
+        database = self.db_param["name"]
         self.connection = sq.connect(database)
         self.cursor = self.connection.cursor()
+        # self.cursor.execute("PRAGMA foreign_keys = ON")
 
     def create_table(self, table="PARAMETERS"):
         keys = ", ".join(
@@ -77,32 +101,38 @@ class Quasi1dData():
         self.cursor.execute(request)
         self.connection.commit()
 
-    def save_interactions(self, **parameters):
+    def create_all(self):
+        for table in self.db_param['table'].keys():
+            self.create_table(table=table)
+
+    def save_data(self, table, data):
+        rows = {r.split()[0] for r in self.db_param['table'][table]}
         assert(
-            set(parameters).issubset(
-                set(self.interactions.keys())
-            )
+            set(data).issubset(rows)
         )
-        params = list(parameters.keys())
-        val = ["?"] * len(params)
-        values = tuple(parameters[k] for k in params)
+        val = ["?"] * len(rows)
+        values = tuple(data[k] for k in rows)
         request = f"""
-            INSERT INTO INTERACTIONS
-            ({",".join(params)})
+            INSERT INTO {table}
+            ({",".join(rows)})
             VALUES ({",".join(val)})
         """
+        # print(values)
+        # # input(request)
+
         self.cursor.execute(request, values)
 
-
-    def save_parameters(self, **parameters):
+    def save_parameters(self, parameters):
         assert(
             set(parameters).issubset(
                 set(self.parameters.keys())
             )
         )
+
         done = True
+        parameters['remarque'] = 'scipy.integrate.solve_ivp'
         parameters_id = self.get_parameters_id(**parameters)
-        # input(parameters_id)
+        # input(f"idp = {parameters_id}")
         if parameters_id != -1:
             return not done, parameters_id[0]
         else:
@@ -114,10 +144,12 @@ class Quasi1dData():
                 ({",".join(params)})
                 VALUES ({",".join(val)})
             """
+
+            # input(request)
             self.cursor.execute(request, values)
             self.connection.commit()
+            # input(self.get_parameters_id(**parameters))
             return done, self.get_parameters_id(**parameters)[0]
-        return (False, 1)
 
     def get_parameters_id(self, **parameters):
 
@@ -126,12 +158,14 @@ class Quasi1dData():
             condition = f"TRUE    "
         else:
             for param, value in parameters.items():
-                if value is not None: # and param != "time":
+                if value is not None and param != 'remarque':  # and param != "time":
                     condition += f"{param}={value} and "
+            condition += f'remarque="{parameters["remarque"]}"'
         request = f"""
             SELECT parametersId FROM PARAMETERS WHERE
-            {condition[:-4]}
+            {condition}
         """
+        # input(request)
         response = self.cursor.execute(request)
         parameters_id = response.fetchall()
         if len(parameters_id) == 0:
@@ -158,7 +192,28 @@ class Quasi1dData():
             {condition}
             order by {",".join(selected_names)}
         """
-        print(request)
+        result = self.cursor.execute(request)
+        return selected_names, result.fetchall()
+
+    def get_data(self, *selected_names, table="PARAMETERS", **parameters):
+        if parameters == {}:
+            condition = "TRUE"
+        else:
+            condition = ""
+            for param, value in parameters.items():
+                condition += f"{param} {value[0]} {value[1]} and "
+            condition = condition[:-4]
+        if selected_names == ():
+            selected_names = [k[:k.index(" ")]
+                              for k in self.db_param["table"][table]
+                              ]
+        request = f"""
+            SELECT {",".join(selected_names)}
+            FROM {table} WHERE
+            {condition}
+        """
+        # order by {",".join(selected_names)}
+        # # input(request)
         result = self.cursor.execute(request)
         return selected_names, result.fetchall()
 
@@ -172,47 +227,91 @@ class Quasi1dData():
 
 
 def update_parameters(db, file):
-    parameters={}
-    idT={}
-    data = np.load(file,allow_pickle=True)[()]
+    interaction = {}
+    susceptibilities = {}
+    parameters = {}
+    idT = {}
+    data = np.load(file, allow_pickle=True)[()]
     parameters.update(data["param"])
     del data["param"]
+
+    parameters['rel_tol'] = data['rel_tol']
+    del data['rel_tol']
+
     if "time" in data:
         timep = data["time"]
         del data["time"]
     else:
         timep = None
+    # # input(data.keys())
     for T in data.keys():
-        parameters["Temperature"]= T
+        parameters["Temperature"] = T
         try:
             parameters["time"] = data[T]["time"]
             del data[T]["time"]
         except:
             parameters["time"] = timep
-        if not isinstance(data[T], list):
-            data[T] = data[T]["g"]
-        # input(type(data[T]))
 
-        idT[T] = [db.save_parameters(**parameters), parameters["Np"]]
-    return idT, data
+        interaction[T] = data[T]["interaction"]
+        del data[T]["interaction"]
+        susceptibilities[T] = data[T]
+        idT[T] = [db.save_parameters(parameters), parameters["Np"]]
+    return idT, interaction, susceptibilities
 
 # @jit
+
+
 def update_interaction(db, idT, data):
-    Np = data
-    values={}
+    all_values = []
+    values = {}
     for T in idT.keys():
         if idT[T][0][0]:
             values["parametersId"] = idT[T][0][1]
             Np = int(idT[T][1])
-            # input(data[T])
+            print(T)
             for i in range(Np):
                 for j in range(Np):
                     for k in range(Np):
-                        values["g1"] = data[T][0][i,j,k]
-                        values["g2"] = data[T][1][i,j,k]
-                        values["g3"] = data[T][2][i,j,k]
-                        db.save_interactions(**values)
-            db.connection.commit()
+                        values["g1"] = data[T]["g1"][i, j, k]
+                        values["g2"] = data[T]["g2"][i, j, k]
+                        values["g3"] = data[T]["g3"][i, j, k]
+                        # # input(values['g1'])
+                        # db.save_data('INTERACTION', values)
+                        all_values.append(values.copy())
+
+    # def execute(val):
+    #     print(val)
+    #     db.save_data(val)
+    #     print('==========================')
+    #     return true
+
+    # with ccf.ThreadPoolExecutor(max_workers=8) as executor:
+    #     # for i in range(8):
+    #     #     update = executor.submit(execute, all_values[i])
+
+    update = [db.save_data('INTERACTION', v) for v in all_values]
+    db.connection.commit()
+
+
+def update_susceptibility(db, idT, data):
+    all_values = []
+    values = {}
+    for T in idT.keys():
+        if idT[T][0][0]:
+
+            values["parametersId"] = idT[T][0][1]
+            for v in data[T]['susc'].values():
+                values.update(v)
+            n_tmp = {}
+            for kvi in values.keys():
+                k = kvi.replace('(', '_')
+                k = k.replace(')', '')
+                n_tmp[k] = values[kvi]
+            all_values.append(n_tmp.copy())
+    update = [db.save_data('SUSCEPTIBILITY', v) for v in all_values]
+    db.connection.commit()
+
+
 def main(db, _O):
     from glob import glob
     import os
@@ -221,40 +320,60 @@ def main(db, _O):
     tini = time.time()
     if os.path.exists(_O.data_file):
         if os.path.isfile(_O.data_file):
-            id_param_T, data = update_parameters(db, _O.data_file)
+            id_param_T, interaction, susceptibilities = update_parameters(
+                db, _O.data_file
+            )
+            update_susceptibility(db, id_param_T, susceptibilities)
+            update_interaction(db, id_param_T, interaction)
     else:
         files = glob("*.npy")
-        # input(files)
+        # # input(files)
         for f in files:
-            id_param_T, data = update_parameters(db, f)
-    update_interaction(db, id_param_T, data)
+            id_param_T, interaction, susceptibilities = update_parameters(
+                db, f)
+            update_susceptibility(db, id_param_T, susceptibilities)
+            update_interaction(db, id_param_T, interaction)
+
+    return interaction, id_param_T
+
     print(f"temps_exec = {time.time()-tini}")
 
+
 if __name__ == "__main__":
+    import random
     _O = opt()
     db = Quasi1dData(database=_O.database)
+    db.create_all()
+    data, idT = main(db, _O)
 
-    # db.drop_db()
-    # db.create_table("PARAMETERS")
-    # db.create_table("INTERACTIONS")
-    # main(db, _O)
-    # main(db, _O)
+    TT = random.choice(list(idT.keys()))
+    print(TT)
+    condition = {
+        "parametersId": ['=', idT[TT][0][1]]
+    }
+    # con = sq.connect('data/quasi1d.db')
+    # c = con.cursor()
+    # c.execute('pragma encoding=UTF16')
+    # c.execute('select * from PARAMETERS').fetchone()
 
-    selected_names, result = db.get_parameters("tp2","Temperature",table="PARAMETERS")
+    selected_names, result = db.get_data(
+        "interactionId", "g1", table="INTERACTION", **condition)
 
-    # g1 = np.zeros((32,32,32), float)
-    # ir=0
-    # for i in range(32):
-    #     for j in range(32):
-    #         for k in range(32):
-    #             g1[i,j,k] = result[ir][0]
-    #             ir += 1
-    # print(np.all(g1 - data[100][1]==0.0))
+    g_t = {i: g for i, g in result}
+    g1 = np.zeros((32, 32, 32), float)
+    ir = min(g_t)
+    for i in range(32):
+        for j in range(32):
+            for k in range(32):
+                g1[i, j, k] = g_t[ir]
+                # # input(f"{g1[i, j, k]},  {data[100]['g1'][i, j, k]}")
+                # assert ir == result[ir][1]
+                ir += 1
+    print(np.all(g1 - data[TT]['g1'] == 0.0))
 
-
-    str_result = ""
-    for r in result:
-        for u, v in zip(selected_names,r):
-            str_result += f"{u}={v} ,"
-        str_result += "\n ========================\n"
-    print(str_result)
+    # str_result = ""
+    # for r in result:
+    #     for u, v in zip(selected_names,r):
+    #         str_result += f"{u}={v} ,"
+    #     str_result += "\n ========================\n"
+    # print(str_result)
